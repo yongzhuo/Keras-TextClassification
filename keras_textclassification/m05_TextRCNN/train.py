7ff2e98750468f97142bb044ad9447c3e8c9eb5d
yongzhuo <31341349+yongzhuo@users.noreply.github.com>
2019-06-23 11:59:42 +0800

    Update train.py

--- keras_textclassification/m05_TextRCNN/train.py
+++ keras_textclassification/m05_TextRCNN/train.py
@@ -11,43 +11,45 @@ project_path = str(pathlib.Path(os.path.abspath(__file__)).parent.parent)
 sys.path.append(project_path)
 print(project_path)
 
+from keras_textclassification.conf.path_config import path_embedding_random_char, path_hyper_parameters_fast_text
 from keras_textclassification.conf.path_config import path_baidu_qa_2019_train, path_baidu_qa_2019_valid
 from keras_textclassification.conf.path_config import path_model_fast_text_baiduqa_2019
-from keras_textclassification.conf.path_config import path_embedding_random_char
 from keras_textclassification.etl.text_preprocess import PreprocessText
 from keras_textclassification.m05_TextRCNN.graph import RCNNGraph as Graph
 
 import random
 
 if __name__=="__main__":
-    hyper_parameters = {'model': {   'label': 17,
-                                     'batch_size': 64,
-                                     'embed_size': 30,
-                                     'filters': [2, 3, 4],
-                                     'kernel_size': 30,
-                                     'channel_size': 1,
-                                     'dropout': 0.5,
-                                     'decay_step': 100,
-                                     'decay_rate': 0.9,
-                                     'epochs': 20,
-                                     'len_max': 50,
-                                     'vocab_size': 20000, #这里随便填的，会根据代码里修改
-                                     'lr': 1e-3,
-                                     'l2': 1e-6,
-                                     'activate_classify': 'softmax',
-                                     'embedding_type': 'random', # 还可以填'random'、 'bert' or 'word2vec"
-                                     'is_training': True,
-                                     'model_path': path_model_fast_text_baiduqa_2019,
-                                     'rnn_type': 'GRU', # type of rnn, select 'LSTM', 'GRU', 'CuDNNGRU', 'CuDNNLSTM', 'Bidirectional-LSTM', 'Bidirectional-GRU'
-                                     'rnn_units': 256,  # large 650, small is 300
-                                     },
-                        'embedding':{ 'embedding_type': 'random',
+    hyper_parameters = {'model': {'label': 17,
+                                  'batch_size': 64,
+                                  'embed_size': 300,
+                                  'filters': [2, 3, 4, 5],  # 这里无用
+                                  'filters_num': 300,
+                                  'channel_size': 1,
+                                  'dropout': 1,
+                                  'decay_step': 100,
+                                  'decay_rate': 0.9,
+                                  'epochs': 20,
+                                  'len_max': 50,
+                                  'vocab_size': 20000,  # 这里随便填的，会根据代码里修改
+                                  'lr': 1e-3,
+                                  'l2': 1e-6,
+                                  'activate_classify': 'softmax',
+                                  'embedding_type': 'random',  # 还可以填'random'、 'bert' or 'word2vec"
+                                  'is_training': True,
+                                  'model_path': path_model_fast_text_baiduqa_2019,
+                                  'path_hyper_parameters': path_hyper_parameters_fast_text,
+                                  'rnn_type': 'GRU',
+                                  # type of rnn, select 'LSTM', 'GRU', 'CuDNNGRU', 'CuDNNLSTM', 'Bidirectional-LSTM', 'Bidirectional-GRU'
+                                  'rnn_units': 256,  # large 650, small is 300
+                                  },
+                        'embedding': {'embedding_type': 'random',
                                       'corpus_path': path_embedding_random_char,
                                       'level_type': 'char',
-                                      'embed_size': 30,
+                                      'embed_size': 300,
                                       'len_max': 50,
                                       },
-                         }
+                        }
     graph = Graph(hyper_parameters)
     ra_ed = graph.word_embedding
     pt = PreprocessText()
@@ -62,3 +64,4 @@ if __name__=="__main__":
     # graph.fit(x_train[0:32000], y_train[0:32000], x_val[0:3200], y_val[0:3200])
 
     graph.fit(x_train, y_train, x_val, y_val)
+    
