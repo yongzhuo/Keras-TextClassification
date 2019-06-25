6ea8d6e5e42dd94d1dd33bdad874b694c164e39f
yongzhuo <31341349+yongzhuo@users.noreply.github.com>
2019-06-25 11:36:10 +0800

    Add files via upload

---
+++ keras_textclassification/m11_SelfAttention/graph.py
@@ -0,0 +1,44 @@
+# -*- coding: UTF-8 -*-
+# !/usr/bin/python
+# @time     :2019/6/3 10:51
+# @author   :Mo
+# @function :()
+
+
+from keras import regularizers
+from keras.layers import Dense
+from keras.layers import Dropout, Flatten
+from keras.layers import SpatialDropout1D, GlobalMaxPooling1D, MaxPooling1D
+from keras.models import Model
+
+from keras_textclassification.keras_layers.Attention_self import AttentionSelf
+from keras_textclassification.base.graph import graph
+
+
+class SelfAttentionGraph(graph):
+    def __init__(self, hyper_parameters):
+        """
+            初始化
+        :param hyper_parameters: json，超参
+        """
+        self.dropout_spatial = hyper_parameters['model'].get('droupout_spatial', 0.2)
+        super().__init__(hyper_parameters)
+
+    def create_model(self, hyper_parameters):
+        """
+            构建神经网络
+        :param hyper_parameters:json,  hyper parameters of network
+        :return: tensor, moedl
+        """
+        super().create_model(hyper_parameters)
+        x = self.word_embedding.output
+        x = SpatialDropout1D(self.dropout_spatial)(x)
+        x = AttentionSelf(self.word_embedding.embed_size)(x)
+        x = GlobalMaxPooling1D()(x)
+        x = Dropout(self.dropout)(x)
+        # x = Flatten()(x)
+        # 最后就是softmax
+        dense_layer = Dense(self.label, activation=self.activate_classify)(x)
+        output = [dense_layer]
+        self.model = Model(self.word_embedding.input, output)
+        self.model.summary(120)
